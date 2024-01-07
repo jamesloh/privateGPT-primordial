fa1399adc7dfd5c4b2ce40158467f4c940ea6902
James Loh <jamesloh.mba@gmail.com>
2024-01-07 23:21:46 +1100

    Use ollama

--- privateGPT.py
+++ privateGPT.py
@@ -4,7 +4,8 @@ from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
-from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms import GPT4All, LlamaCpp, Ollama
+
 import chromadb
 import os
 import argparse
@@ -18,6 +19,8 @@ if not load_dotenv():
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 
+ollama_llm = os.environ.get('OLLAMA_LLM')
+ollama_base_url = os.environ.get('OLLAMA_BASE_URL')
 model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
@@ -29,21 +32,14 @@ from constants import CHROMA_SETTINGS
 def main(log_fp):
     # Parse the command line arguments
     args = parse_arguments()
+
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
     chroma_client = chromadb.PersistentClient(settings=CHROMA_SETTINGS , path=persist_directory)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS, client=chroma_client)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-    # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    # Prepare the LLM
-    match model_type:
-        case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case "GPT4All":
-            llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case _default:
-            # raise exception if model_type is not supported
-            raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+
+
+    llm = Ollama(model=ollama_llm, base_url=ollama_base_url, temperature=0)
 
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
     # Interactive questions and answers
@@ -53,6 +49,10 @@ def main(log_fp):
             break
         if query.strip() == "":
             continue
+
+        answer = llm(query)
+        print(answer)
+        print(answer, file=log_fp)
 
         # Get the answer from the chain
         start = time.time()
